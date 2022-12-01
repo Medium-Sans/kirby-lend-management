dc57a623b7a88ebc9a4a093f73ee5bc419cd8d3e
Samuel Cardoso <R3D2@users.noreply.github.com>
2022-12-01 13:29:10 +0100

    Add update dialog

--- dialogs/borrower/update.php
+++ dialogs/borrower/update.php
@@ -1,1 +1,22 @@
 <?php
+
+use Kirby\LendManagement\Borrower;
+
+return [
+    'pattern' => 'lendmanagement/borrower/(:any)/update',
+    'load'    => function (string $id) {
+        $borrower = Borrower::find($id);
+
+        return [
+            'component' => 'k-form-dialog',
+            'props' => [
+                'fields' => require __DIR__ . '/fields.php',
+                'value'  => $borrower,
+                'size'   => 'large'
+            ]
+        ];
+    },
+    'submit' => function (string $id) {
+        return Borrower::update($id, get());
+    }
+];
