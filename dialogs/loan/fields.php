dc6cec9dd05711f77898c2ff42975e11898126b1
Samuel Cardoso <R3D2@users.noreply.github.com>
2022-12-02 10:42:52 +0100

    Modify requirements for the fields in the loan view

--- dialogs/loan/fields.php
+++ dialogs/loan/fields.php
@@ -10,24 +10,29 @@ return [
         'label' => 'Date de début',
         'type' => 'date',
         'time' => false,
+        'required' => true,
         'width' => '1/2'
     ],
     'endDate' => [
         'label' => 'Date de fin',
         'type' => 'date',
         'time' => false,
+        'required' => true,
         'width' => '1/2'
     ],
     'borrowerId' => [
         'label' => 'Emprunteur.se.x',
         'type' => 'multiselect',
+        'max' => 1,
         'search' => true,
+        'required' => true,
         'options' => Borrower::getOptions(),
     ],
     'itemIds' => [
         'label' => 'Objets',
         'type' => 'multiselect',
         'search' => true,
+        'required' => true,
         'options' => Item::getOptions(),
     ]
 ];
