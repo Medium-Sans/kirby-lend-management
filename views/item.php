8b233c16bf44efb91735956fc4d91492389b336e
Samuel Cardoso <R3D2@users.noreply.github.com>
2022-12-02 10:39:55 +0100

    Add item view handler

---
+++ views/item.php
@@ -0,0 +1,32 @@
+<?php
+
+use Kirby\LendManagement\Category;
+use Kirby\LendManagement\Item;
+
+return [
+    'pattern' => 'lendmanagement/inventory/item/(:any)',
+    'action'  => function ($id) {
+
+        $item = Item::find($id);
+        $categories = Category::getOptions();
+
+        return [
+            'component' => 'k-item-view',
+            'extends' => 'k-form',
+            'breadcrumb' => [
+                [
+                    'label' => 'Inventaire',
+                    'link'  => 'lendmanagement/inventory'
+                ],
+                [
+                    'label' => $item['title'],
+                    'link'  => 'lendmanagement/inventory/item/' . $id
+                ]
+            ],
+            'props' => [
+                'item'          => $item,
+                'categories'    => $categories,
+            ],
+        ];
+    }
+];
