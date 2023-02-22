c4bc8bc835f7a1933db79540dda91051de536029
Samuel Cardoso <R3D2@users.noreply.github.com>
2023-02-22 15:07:50 +0100

    Add routes

--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,9 @@
 <?php
 
+use Kirby\Http\Response;
 use Kirby\LendManagement\Item;
 use Kirby\LendManagement\Loan;
+use Kirby\Data\Data;
 
 return [
     'routes' => function () {
@@ -19,7 +21,37 @@ return [
                 'action' => function () {
                     return Loan::create(get());
                 }
-            ]
+            ],
+            [
+                'pattern' => 'lendmanagement/loan/(:any)/return',
+                'method' => 'POST',
+                'action' => function (string $id) {
+                    return Loan::return($id);
+                }
+            ],
+            [
+                'pattern' => 'lendmanagement/loan/(:any)/extend',
+                'method' => 'POST',
+                'action' => function (string $id) {
+                    return Loan::return($id);
+                }
+            ],
+            [
+                'pattern' => 'lendmanagement/loan/(:any)/notify',
+                'method' => 'POST',
+                'action' => function (string $id) {
+                    return Loan::return($id);
+                }
+            ],
+            [
+                'pattern' => 'lendmanagement/item/(:any)/print',
+                'method' => 'POST',
+                'action' => function (string $id) {
+                    $label = Item::getLabelFromItemId($id);
+                    $encoded = base64_encode(trim($label));
+                    return response::json([ 'data' => $encoded ]);
+                }
+            ],
         ];
     }
 ];
