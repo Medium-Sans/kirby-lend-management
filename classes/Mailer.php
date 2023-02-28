72fe58d05a7870daddfc7335aaf3f4ea5e1c70d7
Samuel Cardoso <R3D2@users.noreply.github.com>
2023-02-28 16:25:54 +0100

    Add notification

--- classes/Mailer.php
+++ classes/Mailer.php
@@ -2,7 +2,29 @@
 
 namespace Kirby\LendManagement;
 
+use Kirby\Exception\Exception;
+
 class Mailer
 {
-    # Todo: Implement Mailer class
+    static function notifyLenderOfExpiration(\stdClass $lend): bool
+    {
+        $borrower = Borrower::find($lend->borrower_id);
+        $kirby = kirby();
+        $sent = true;
+
+        try {
+            $kirby->email([
+                'to' => $borrower->email,
+                'from' => 'notifications@head-pool-image-son.ch',
+                'body' => 'L\'emprunt effectuer au pool image-son est arrivé à échéance, merci de ramener le matériel le mardi suivant de 9h à 12h',
+                'replyTo' => 'samuel.cardoso@hesge.ch',
+                'subject' => 'Emprunt - Pool Image-Son',
+            ]);
+        } catch (Exception $error) {
+            echo $error;
+            $sent = false;
+        }
+
+        return $sent;
+    }
 }
